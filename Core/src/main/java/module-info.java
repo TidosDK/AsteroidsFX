45bc359c732aacda149d178ca485eeb187932112
TidosDK <28749155+TidosDK@users.noreply.github.com>
2024-05-11 02:42:17 +0200

    Spring implementation

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -6,7 +6,11 @@ module Core {
     requires Common;
     requires CommonBullet;
     requires javafx.graphics;
-    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    exports dk.sdu.mmmi.cbse.main;
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics, spring.core;
     uses IGamePluginService;
     uses IEntityProcessingService;
     uses IPostEntityProcessingService;
